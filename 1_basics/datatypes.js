61a532411f8c588eb4769b9c20c4281d86f00be8
ismailgithubrepo <165195430+ismailgithubrepo@users.noreply.github.com>
2024-04-24 13:02:23 +0000

    learning datatype

---
+++ 1_basics/datatypes.js
@@ -0,0 +1,18 @@
+"use strict";
+let isloggin = true;
+let age = 12
+let shoesize=8.5 //no float in js
+let name="ismail";
+let height=undefined
+let bus = null
+console.log(typeof isloggin)
+console.log(typeof age)
+console.log(typeof name)
+console.log(typeof shoesize)
+console.log(typeof "shaik")
+console.log(typeof 34)
+console.log(typeof 56.9)
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof height)
+console.log(typeof null)
